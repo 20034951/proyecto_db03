e4c0029a08d11b5efcd2256031302e909d194117
20034951 <20034951@galileo.edu>
2025-03-17 17:41:46 -0600

    Agregando control de transacciones a Script Fase 01 del Proyecto

--- proyecto_db03_fase01.sql
+++ proyecto_db03_fase01.sql
@@ -36,6 +36,23 @@ CREATE TABLE productos (
     FOREIGN KEY (ubicacion_id) REFERENCES ubicaciones(id_ubicacion)
 );
 
+CREATE TABLE tipos_transaccion (
+	id_tipo INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(60) UNIQUE NOT NULL
+);
+
+CREATE TABLE transacciones (
+	id_transaccion INT AUTO_INCREMENT PRIMARY KEY,
+    producto_id INT NOT NULL,
+    cambio_stock INT NOT NULL,
+    tipo_id INT NOT NULL,
+    usuario_id INT NOT NULL,
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (producto_id) REFERENCES productos(id_producto),
+    FOREIGN KEY (tipo_id) REFERENCES tipos_transaccion(id_tipo),
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id_usuario)
+);
+
 
 -- Creación de Store Procedures
 
@@ -134,9 +151,24 @@ CREATE PROCEDURE ActualizarProducto(
     IN p_descripcion TEXT,
     IN p_precio DECIMAL(10,2),
     IN p_stock INT,
-    IN p_ubicacion_id INT
+    IN p_ubicacion_id INT,
+    IN p_usuario_id INT
 )
 BEGIN
+	DECLARE stock_anterior INT;
+    DECLARE tipo_transaccion INT;
+    DECLARE stock_cambio INT;
+    
+    SELECT stock INTO stock_anterior FROM productos WHERE id_producto = p_id_producto;
+    
+    IF stock_anterior < p_stock THEN
+		SET tipo_transaccion = 1;
+        SET stock_cambio = p_stock - stock_anterior;
+	ELSEIF stock_anterior > p_stock THEN
+		SET tipo_transaccion = 2;
+		SET stock_cambio = stock_anterior - p_stock;
+	END IF;
+
 	UPDATE productos 
     SET 
     codigo = p_codigo, 
@@ -146,6 +178,12 @@ BEGIN
     stock = p_stock, 
     ubicacion_id = p_ubicacion_id
     WHERE id_producto = p_id_producto;
+    
+    IF stock_anterior <> p_stock THEN
+		INSERT INTO transacciones (producto_id, cambio_stock, tipo_id, usuario_id)
+        VALUES (p_id_producto, stock_cambio, tipo_transaccion, p_usuario_id);
+	END IF;
+    
 END $$
 DELIMITER ;
 
@@ -213,6 +251,33 @@ BEGIN
 END $$
 DELIMITER ;
 
+-- Reporte de Productos por rango de precio
+DELIMITER $$
+CREATE PROCEDURE ReporteProductosPorRangoDePrecio (
+	IN p_precio_min DECIMAL(10,2),
+    IN p_precio_max DECIMAL(10,2)
+)
+BEGIN
+	SELECT p.id_producto, p.codigo, p.nombre, p.stock, p.precio, (p.stock * p.precio) AS valor_total, u.descripcion AS ubicacion
+    FROM productos p
+    JOIN ubicaciones u ON p.ubicacion_id = u.id_ubicacion
+    WHERE p.precio BETWEEN p_precio_min AND p_precio_max;
+END $$
+DELIMITER ;
+
+-- Reporte de Transacciones
+DELIMITER $$
+CREATE PROCEDURE ReporteTransacciones ()
+BEGIN
+	SELECT p.codigo, p.nombre, p.stock, t.cambio_stock, tt.nombre
+    FROM transacciones t
+    LEFT JOIN productos p ON p.id_producto = t.producto_id
+    LEFT JOIN tipos_transaccion tt ON tt.id_tipo = t.tipo_id;
+END $$
+DELIMITER ;
+
+
+
 
 -- Creación de Registros simulados
 DELIMITER $$
@@ -225,6 +290,7 @@ BEGIN
 	END WHILE;
     
     INSERT INTO roles (nombre) VALUES ('admin'), ('almacen'), ('vendedor');
+    INSERT INTO tipos_transaccion(nombre) VALUES ('entrada'), ('salida');
     
     SET i = 1;
     WHILE i <= 500 DO
@@ -258,3 +324,20 @@ CALL ReporteProductosPorIdUbicacion(5);
 CALL ReporteProductosBajoStock(20);
 
 CALL ReporteProductosPorRangoDePrecio(50.00, 100.00);
+
+
+
+SELECT * FROM productos LIMIT 5;
+
+
+CALL ActualizarProducto(1, 'P-0001', 'Producto 1', 'Descripción 1', 70.61, 22, 5, 1);
+CALL ActualizarProducto(1, 'P-0001', 'Producto 1', 'Descripción 1', 70.61, 20, 5, 1);
+
+CALL ActualizarProducto(2, 'P-0002', 'Producto 2', 'Descripción 2', 54.55, 30, 4, 1);
+CALL ActualizarProducto(2, 'P-0002', 'Producto 2', 'Descripción 2', 54.55, 28, 4, 1);
+
+CALL ActualizarProducto(3, 'P-0003', 'Producto 3', 'Descripción 3', 57.51, 80, 3, 1);
+CALL ActualizarProducto(3, 'P-0003', 'Producto 3', 'Descripción 3', 57.51, 90, 3, 1);
+
+
+CALL ReporteTransacciones();
